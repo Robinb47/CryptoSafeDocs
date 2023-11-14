42a3e2e88e800fbc5c6884388c13de96cedb0b53
Robinb47 <63455335+Robinb47@users.noreply.github.com>
2023-11-14 21:04:48 +0100

    upload form with fetch to server

--- client/reactDapp/src/App.jsx
+++ client/reactDapp/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [file, setFile] = useState(null);
+  const [ipfsHash, setIpfsHash] = useState(''); // Neuer Zustand für den IPFS-Hash
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleFileUpload = () => {
+    if (file) {
+      const formData = new FormData();
+      formData.append('pdfFile', file);
+
+      fetch('http://localhost:5040/upload', {
+        method: 'POST',
+        body: formData,
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json(); // Parsen der JSON-Antwort
+          } else {
+            console.error('Error uploading PDF:', response.statusText);
+          }
+        })
+        .then((data) => {
+          if (data.ipfsHash) {
+            setIpfsHash(data.ipfsHash); // Setzen des IPFS-Hash im Zustand
+          }
+        })
+        .catch((error) => {
+          console.error('Error uploading PDF:', error);
+        });
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <input type="file" onChange={handleFileChange} accept=".pdf" />
+      <button onClick={handleFileUpload}>Upload PDF</button>
+      {ipfsHash && <p>IPFS-Hash: {ipfsHash}</p>} {/* Zeige den IPFS-Hash an */}
+    </div>
+  );
+};
 
-export default App
+export default App;
