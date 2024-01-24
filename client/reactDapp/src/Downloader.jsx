4a9679b8e89d261a31f10189b1c84786cfcd61fb
Robinb47 <63455335+Robinb47@users.noreply.github.com>
2024-01-24 20:00:08 +0100

    Design updates

--- client/reactDapp/src/Downloader.jsx
+++ client/reactDapp/src/Downloader.jsx
@@ -1,17 +1,14 @@
 import React, { useEffect, useState } from 'react';
-
 import { ethers } from 'ethers'
-//import forge from 'node-forge';
 
-//import RSAPublicKeyRegistry_abi from './contracts/RSAPublicKeyRegistry_abi.json';
-//import { setegid } from 'process';
-
-//import EncryptedLinks_abi from './contracts/EncryptedLinks_abi.json';
-//import { on } from 'events';
-
+//Smart Contract abi import
 import KeyAndLinkMananger_abi from './contracts/KeyAndLinkManager_abi.json';
 
 function Downloader() {
+
+     //set here your contract address after deploying via Remix
+    let contractAddress = "0x5dcD32D9F30999D537695B2029579481540392e2";
+
 
     //Erweiterung für Smart Contract Interaktion
     const [provider, setProvider] = useState("");
@@ -21,21 +18,24 @@ function Downloader() {
     const [decryptedDocument, setDecryptedDocument] = useState(""); 
     const [defaultAccount, setDefaultAccount] = useState("");
 
-    let contractAddress = "0x5dcD32D9F30999D537695B2029579481540392e2";
     
-    //ich wollte dies im Hintergrund mit folgenden Code laufen lassen, es buggt dann aber leider 
+    //request connected crypto-wallet from MetaMask extension
     const connectWalletHandler = () => {
         window.ethereum.request({method: 'eth_requestAccounts'})
         .then(result => {
             accountChangedHandler(result[0]);
         })
-      }
-    
+    }
+
+    /**
+     * This method changes the requested account and repeats the connection with new account.
+     */
     const accountChangedHandler = (newAccount) => {
         setDefaultAccount(newAccount);
         contractConnect();
     }
 
+    //Smart Contract connection with contractAddress and abi
     const contractConnect = async () => {
         let tempProvider = new ethers.BrowserProvider(window.ethereum);
         setProvider(tempProvider);
@@ -47,48 +47,19 @@ function Downloader() {
         setContract(tempContract);
     }
 
-
+    //asking saved public key from recipient on blockchain
     const getCryptedLink = async () => {
         let encryptedLink = await contract.getDocument();
         setEncryptedDocument(encryptedLink);
     }
 
-    //OG Version
-    const handleDecryption = async () => {
-        //const formData = new FormData();
-        let encrypted = await contract.getDocument();
-        //let encrypted = "hallo";
-       // formData.append('encryptedDocument', encrypted);
-        console.log("verschlüsselter Hashwert: ", encrypted);
-
-        fetch('http://localhost:5052/download', {
-            method: 'POST',
-            body: encrypted,
-            headers: {
-                'Content-Type': 'text/plain',
-            }
-            })
-            .then((response) => {
-                if (response.ok) {
-                   // setDecryptedDocument(response.text());
-                    return response.text();
-                } else {
-                    console.error('Error decrypting the Document ');
-                    throw new Error('Download failed');
-                }
-            })
-            .then((decryptedText) => {
-                //Hier setzt du den entschlüsselten Text ein
-                setDecryptedDocument(decryptedText);
-            })
-            .catch((error) => {
-                //Handle Fetch error
-                console.error('Fetch error:', error);
-            });
-    }
-
-    //passage Version mit WebAuthn
-    const handleDecryptionPassage = async () => {
+    /**
+     * This method is asking the saved encrypted hash from blockchain.
+     * The smart contract checks the address from user and returns his hash,
+     * which has been setting before for him.
+     * Additionaly it checks the WebAuthn auth-token.
+     */
+        const handleDecryptionPassage = async () => {
         let encrypted = await contract.getDocument();
         console.log("verschlüsselter Hashwert: ", encrypted);
 
@@ -114,26 +85,12 @@ function Downloader() {
                 }
             })
             .then((decryptedText) => {
-                //Hier setzt du den entschlüsselten Text ein
                 setDecryptedDocument(decryptedText);
             })
             .catch((error) => {
                 //Handle Fetch error
                 console.error('Fetch error:', error);
             });
-    }
-
-    //PassageAuthTest
-    const handlePassage = async () => {
-
-        fetch('http://localhost:5052/authenticatedRoute', {
-            method: 'POST',
-            body: "hallo",
-            headers: {
-                'Content-Type': 'text/plain',
-            }
-        });
-
     }
 
     return (
@@ -147,7 +104,7 @@ function Downloader() {
             <button onClick={handleDecryptionPassage}>click to decrypt ipfs-Link</button>
             <p>decrypted IPFS-Link: {decryptedDocument}</p>
 
-            <button onClick={handlePassage}>Try Passage SDK</button>
+        
         </div>
     );
     
